83a390cdd2c27a4a268608f0bca122cee6d0f39f
Silvan Spross <silvan.spross@gmail.com>
2013-11-16 14:40:46 +0100

    started learning raycasting

---
+++ Assets/Scripts/LittleRaycaster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LittleRaycaster : MonoBehaviour {
+
+	public bool blockedRight = false;
+
+	RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 rightOfSelf = transform.TransformDirection(Vector3.right);
+		float halfSelf = transform.collider.bounds.size.x / 2;
+
+		Vector3 selfTop = new Vector3(transform.position.x, transform.position.y + halfSelf, transform.position.z);
+		Vector3 selfBottom = new Vector3(transform.position.x, transform.position.y - halfSelf, transform.position.z);
+		Debug.DrawRay(selfTop, rightOfSelf * halfSelf, Color.red);
+		Debug.DrawRay(selfBottom, rightOfSelf * halfSelf, Color.red);
+
+		if (Physics.Raycast(selfTop, rightOfSelf, halfSelf) || Physics.Raycast(selfBottom, rightOfSelf, halfSelf)) {
+			blockedRight = true;
+		} else {
+			blockedRight = false;
+		}
+	}
+}
