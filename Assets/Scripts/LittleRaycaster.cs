b19ba7ff725d4a93a6bc1349f1c9e98e61681612
Silvan Spross <silvan.spross@gmail.com>
2013-11-16 15:31:49 +0100

    worked out right behavior

--- Assets/Scripts/LittleRaycaster.cs
+++ Assets/Scripts/LittleRaycaster.cs
@@ -3,29 +3,88 @@ using System.Collections;
 
 public class LittleRaycaster : MonoBehaviour {
 
-	public bool blockedRight = false;
+	public float fuzzyEdgeFactor = 0.01f;  // if 0 edge on edge connection would set two blocks true
+	public bool blockedRight, blockedLeft, blockedUp, blockedDown = false;
+	public bool showRaycasts = false;
 
+	float halfSelf, edgeCorrection;
+	Vector3 selfTop, selfBottom, selfRight, selfLeft;
 	RaycastHit hit;
 
 	// Use this for initialization
 	void Start () {
-	
+		halfSelf = transform.collider.bounds.size.x / 2;
+		edgeCorrection = transform.collider.bounds.size.x * fuzzyEdgeFactor;  
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 rightOfSelf = transform.TransformDirection(Vector3.right);
-		float halfSelf = transform.collider.bounds.size.x / 2;
+		CheckRaycasts();
+	}
 
-		Vector3 selfTop = new Vector3(transform.position.x, transform.position.y + halfSelf, transform.position.z);
-		Vector3 selfBottom = new Vector3(transform.position.x, transform.position.y - halfSelf, transform.position.z);
-		Debug.DrawRay(selfTop, rightOfSelf * halfSelf, Color.red);
-		Debug.DrawRay(selfBottom, rightOfSelf * halfSelf, Color.red);
+	void CheckRaycasts() {
+		// Update position values
+		selfTop = new Vector3(transform.position.x, transform.position.y + halfSelf - edgeCorrection, transform.position.z);
+		selfBottom = new Vector3(transform.position.x, transform.position.y - halfSelf + edgeCorrection, transform.position.z);
+		selfRight = new Vector3(transform.position.x + halfSelf - edgeCorrection, transform.position.y, transform.position.z);
+		selfLeft = new Vector3(transform.position.x - halfSelf + edgeCorrection, transform.position.y, transform.position.z);
 
-		if (Physics.Raycast(selfTop, rightOfSelf, halfSelf) || Physics.Raycast(selfBottom, rightOfSelf, halfSelf)) {
+		// Check different directions
+		CheckRight();
+		CheckLeft();
+		CheckUp();
+		CheckDown();
+	}
+
+	void CheckRight() {
+		Vector3 direction = transform.TransformDirection(Vector3.right);
+		if (showRaycasts) {
+			Debug.DrawRay(selfTop, direction * halfSelf, Color.cyan);
+			Debug.DrawRay(selfBottom, direction * halfSelf, Color.cyan);
+		}
+		if (Physics.Raycast(selfTop, direction, halfSelf) || Physics.Raycast(selfBottom, direction, halfSelf)) {
 			blockedRight = true;
 		} else {
 			blockedRight = false;
+		}
+	}
+
+	void CheckLeft() {
+		Vector3 direction = transform.TransformDirection(Vector3.left);
+		if (showRaycasts) {
+			Debug.DrawRay(selfTop, direction * halfSelf, Color.cyan);
+			Debug.DrawRay(selfBottom, direction * halfSelf, Color.cyan);
+		}
+		if (Physics.Raycast(selfTop, direction, halfSelf) || Physics.Raycast(selfBottom, direction, halfSelf)) {
+			blockedLeft = true;
+		} else {
+			blockedLeft = false;
+		}
+	}
+
+	void CheckUp() {
+		Vector3 direction = transform.TransformDirection(Vector3.up);
+		if (showRaycasts) {
+			Debug.DrawRay(selfRight, direction * halfSelf, Color.cyan);
+			Debug.DrawRay(selfLeft, direction * halfSelf, Color.cyan);
+		}
+		if (Physics.Raycast(selfRight, direction, halfSelf) || Physics.Raycast(selfLeft, direction, halfSelf)) {
+			blockedUp = true;
+		} else {
+			blockedUp = false;
+		}
+	}
+
+	void CheckDown() {
+		Vector3 direction = transform.TransformDirection(Vector3.down);
+		if (showRaycasts) {
+			Debug.DrawRay(selfRight, direction * halfSelf, Color.cyan);
+			Debug.DrawRay(selfLeft, direction * halfSelf, Color.cyan);
+		}
+		if (Physics.Raycast(selfRight, direction, halfSelf) || Physics.Raycast(selfLeft, direction, halfSelf)) {
+			blockedDown = true;
+		} else {
+			blockedDown = false;
 		}
 	}
 }
